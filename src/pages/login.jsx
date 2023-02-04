047626783267cd0fc55d42a8b33568200931554a
Ivan Shiryaev <ivan.s.shiryaev@yandex.ru>
2023-02-04 13:33:31 +0300

    Add login page and register page layouts and linking

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import {
+    Link,
+} from 'react-router-dom';
+import {
+	EmailInput,
+    PasswordInput,
+    Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+import AppHeader from '../components/app-header/app-header';
+import styles from './login.module.css';
 
 export function LoginPage() {
 
     return (
         <React.Fragment>
-            LoginPage
+            <AppHeader />
+            <main
+                className={styles.wrapper}
+            >
+                <article
+                    className={styles.container}
+                >
+                    <h1
+                        className="text text_type_main-medium"
+                    >
+                        Вход
+                    </h1>
+                    <EmailInput
+                        name={'email'}
+                        placeholder={'E-mail'}
+                        isIcon={false}
+                        extraClass="mt-6"
+                    />
+                    <PasswordInput
+                        name={'password'}
+                        extraClass="mt-6"
+                    />
+                    <Button
+                        htmlType="button"
+                        type="primary"
+                        size="medium"
+                        extraClass="mt-6"
+                    >
+                        Войти
+                    </Button>
+                    <p
+                        className="text text_type_main-default text_color_inactive mt-20"
+                    >
+                        Вы &mdash; новый пользователь? <Link to="/register">Зарегистрироваться</Link>
+                    </p>
+                    <p
+                        className="text text_type_main-default text_color_inactive mt-4"
+                    >
+                        Забыли пароль? <Link to="/forgot-password">Восстановить пароль</Link>
+                    </p>
+                </article>
+            </main>
         </React.Fragment>
     );
 
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,64 @@
 import React from 'react';
+import {
+    Link,
+} from 'react-router-dom';
+import {
+    Input,
+	EmailInput,
+    PasswordInput,
+    Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+import AppHeader from '../components/app-header/app-header';
+import styles from './register.module.css';
 
 export function RegisterPage() {
 
     return (
         <React.Fragment>
-            RegisterPage
+            <AppHeader />
+            <main
+                className={styles.wrapper}
+            >
+                <article
+                    className={styles.container}
+                >
+                    <h1
+                        className="text text_type_main-medium"
+                    >
+                        Регистрация
+                    </h1>
+                    <Input
+                        type="text"
+                        name={'name'}
+                        placeholder={'Имя'}
+                        extraClass="mt-6"
+                    />
+                    <EmailInput
+                        name={'email'}
+                        placeholder={'E-mail'}
+                        isIcon={false}
+                        extraClass="mt-6"
+                    />
+                    <PasswordInput
+                        name={'password'}
+                        extraClass="mt-6"
+                    />
+                    <Button
+                        htmlType="button"
+                        type="primary"
+                        size="medium"
+                        extraClass="mt-6"
+                    >
+                        Зарегистрироваться
+                    </Button>
+                    <p
+                        className="text text_type_main-default text_color_inactive mt-20"
+                    >
+                        Уже зарегистрированы? <Link to="/login">Войти</Link>
+                    </p>
+                </article>
+            </main>
         </React.Fragment>
     );
 
